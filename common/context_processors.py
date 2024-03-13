223012d417595b5deb82716d3d399a9ee1927e2b
WayupKG <adikgk232323@gmail.com>
2024-03-13 23:33:12 +0600

    feat: added CONTACTS for dynamic value

--- common/context_processors.py
+++ common/context_processors.py
@@ -7,4 +7,5 @@ def getting_info(request):
     style_core_version = settings.STYLE_CORE_VERSION
     style_responsive_version = settings.STYLE_RESPONSIVE_VERSION
     edu_processes = EduProcess.active.all()
+    CONTACTS = settings.CONTACTS
     return locals()
